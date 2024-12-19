5c1e64a6e0197a70b8b9a0b5b93bab109b3aa70e
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-12-19 14:15:23 +0800

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import router from '../router';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://localhost:8000',
   timeout: 5000,
   withCredentials: true,
   withXSRFToken: true,
@@ -15,7 +16,7 @@ instance.interceptors.response.use(
   (response) => response,
   (error) => {
       if (error.response && (error.response.status === 401 || error.response.status === 419)) {
-          // localStorage.removeItem('session');
+          localStorage.removeItem('isLoggedIn');
           localStorage.removeItem('reload');
           localStorage.removeItem('role');
           localStorage.removeItem('token');
